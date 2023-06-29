c196c59487c6c60d62f3e73eb700d4c75c1c4a09
Pac-Lap-066 <hemanthsai797@gmail.com>
2023-06-29 14:39:29 +0530

    first

---
+++ python 2806.py
@@ -0,0 +1,56 @@
+# eno=int(input("enter employee number:"))
+# ename=(input("enter employee name:"))
+# esal=float(input("enter employee salary:"))
+# eaddress=(input(" enter employee address:"))
+# estate=bool(input("enter employee marriage status:[True:False]"))
+# print("please confirm information..")
+# print("employee number:",eno)
+# print("employee name:",ename)
+# print("employee salary:",esal)
+# print("employee address:",eaddress)
+# print("employee marriage:",estate)
+
+
+# a,b=[int(x) for x in input("enter 2 numbers:").split()]
+# print("the product:", a*b)
+
+
+# a,b=[float(x) for x in input("enter 2 float numbers:").split(',')]
+# print("the product:", a*b)
+
+
+# a,b,c,d=[float(x) for x in input("enter 4 float numbers:").split(',')]
+# print("the product:", a*b*c*d)
+
+
+# x=eval("10+20+30")
+# print(x)
+
+
+# x=input("enter a expression:")
+# result=eval(x)
+# print("the sum:", result)
+
+
+# x=eval(input("enter some name:"))
+# print(type(x))
+# for x1 in x:
+#     print(x1)
+
+
+# a,b,c=[eval(x) for x in input("enter 3 values:").split(",")]
+# print(type(a))
+# print(type(b))
+# print(type(c))
+
+
+# a,b,c=[eval(x) for x in input("enter 3 values:").split("5")]
+# print(type(a))
+# print(type(b))
+# print(type(c))
+
+# from sys import argv
+# print(type(argv))
+
+from sys import argv
+print(argv)
---
+++ python2906.py
@@ -0,0 +1,71 @@
+# from sys import argv
+# print(argv)
+# print(argv[0])
+# print(argv[1:])
+# print(argv[:])
+
+
+# from sys import argv
+# print("the number of command line arguments:",len(argv))
+# print("the list of command line arguments:",argv)
+# print("command line arguments one by one :")
+# for x in argv:
+#     print(x)
+# print("slice operator result:",argv[1:8])
+
+
+# from sys import argv
+# args=argv[1:]
+# sum=0
+# for x in args:
+#     n=int(x)
+#     sum += n
+# print("the sum:" ,sum)
+
+
+
+# from sys import argv
+# print(argv[1])
+
+
+# from sys import argv
+# print(argv[1]+argv[2])
+
+
+
+# from sys import argv
+# print(int(argv[1])+int(argv[2]))
+
+
+# from sys import argv
+# print(eval(argv[1])+eval(argv[2]))
+
+
+# from sys import argv
+# print(int(argv[1])+int(argv[2]))
+
+
+# from sys import argv
+# print(argv[7:100])
+
+
+# print("hemanth")
+# print()
+# print("good evg")
+# print("submit")
+
+
+
+# print("hello \n hemanth")
+# print("hello \t hemanth")
+
+
+# print("hemanth"*4)
+
+
+
+# print("hemanth"+"kolli")
+# print("hemanth","kolli")
+
+
+
