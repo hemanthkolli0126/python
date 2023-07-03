0194483072848be73517f9af851d1bfa2491a676
Pac-Lap-066 <hemanthsai797@gmail.com>
2023-07-03 16:58:17 +0530

    six

--- python0307.py
+++ python0307.py
@@ -1,6 +1,90 @@
-x=int(input("enter the number:"))
-y=int(input("enter the number:"))
-if x>y:
-    print("bigger no is", x )
-else:
-    print("bigger no is", y )
+# x=int(input("enter the number:"))
+# y=int(input("enter second number:"))
+# if x>y:
+#     print("bigger no is", x )
+# else:
+#     print("bigger no is", y )
+
+
+
+
+# x=eval(input("enter the number:"))
+# y=eval(input("enter second number:"))
+# z=eval(input("enter third number:"))
+# if x>y and x>z:
+#     print("bigger no is:", x )
+# elif y>z:
+#     print("bigger no is:", y )
+# else:
+#     print("bigger no is:", z)
+
+
+
+
+# x=eval(input("enter the number:"))
+# y=eval(input("enter second number:"))
+# z=eval(input("enter third number:"))
+# if x<y and x<z:
+#     print(" smaller is:", x )
+# elif y<z:
+#     print("smaller no is:", y )
+# else:
+#     print("smaller no is:", z)
+
+
+
+
+# n=int(input("enter the number:"))
+# if n>=1 and n<=100:
+#     print("the number is in the range")
+# else:
+#     print("the number is not in the range")
+
+
+
+
+# n=int(input("enter the digit from 0 to 9:"))
+# if n==0:
+#     print("zero")
+# elif n==1:
+#     print("one")
+# elif n==2:
+#     print("two")
+# elif n==3:
+#     print("three")
+# elif n==4:
+#     print("four")
+# elif n==5:
+#     print("five")
+# elif n==6:
+#     print("six")
+# elif n==7:
+#     print("seven")
+# elif n==8:
+#     print("eight")
+# elif n==9:
+#     print("nine")
+# else:
+#     print("enter the number from 0 to 9 only")
+
+
+
+
+# h="hemanth"
+# for x in h:
+#     print(x)
+
+
+
+# h="hemanth"
+# count=0
+# for x in h:
+#     count+=1
+#     print(x)
+# print("the number of letters are:", count)
+
+
+l=[10,20,30,40]
+for x in l:
+    print(x)
+
