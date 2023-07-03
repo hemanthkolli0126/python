253759b47943d0f99badd2b6dcac58db41af34a4
Pac-Lap-066 <hemanthsai797@gmail.com>
2023-07-03 10:33:08 +0530

    five

---
+++ python3006.py
@@ -0,0 +1,25 @@
+# name=input("enter name")
+# if name=="hemanth":
+#     print("hello hemanth good mrng")
+# print("how are you")
+
+
+# name=input("enter name:")
+# if name=="hemanth":
+#     print("hello hemanth good mrng")
+# else:
+#     print("hello guest good mrng")
+# print("how are you")
+
+drink=input("enter your fav drink:")
+if drink=="pepsi":
+    print("its to strong")
+elif drink=="mazza":
+    print("its too sweet")
+elif drink=="limca":
+    print("its to lime")
+elif drink=="coke":
+    print("its has too much caffine")
+else:
+    print("others are not recommended")
+    
