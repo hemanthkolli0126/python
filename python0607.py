654d51a62a806f6beb2ce4c02709e2ca29d90df1
hemanthkolli031 <hemanthsai797@gmail.com>
2023-07-06 18:05:55 +0530

    string

---
+++ python0607.py
@@ -0,0 +1,51 @@
+# s="this is ' single quote symbol"
+# print(s) 
+
+
+
+# s='this is " double quote symbol'
+# print(s)
+
+
+
+# s=""" these are ' and " sybmol """
+# print(s)
+
+
+
+# s="hemanth"
+# print(s[0])
+
+
+
+# s=input("enter some string:")
+# i=0
+# for x in s:
+#     print("the character present at positive index{} and at negative index{} is : {}".format(i,i-len(s),x))
+#     i=i+1
+
+
+
+s="0123456789"
+print(s[0:10:1])
+print(s[0:9:2])
+print(s[0:])
+print(s[::])
+print(s[::-1])
+print(s[::1])
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
