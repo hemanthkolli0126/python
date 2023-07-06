c2e72249b597656b718510ae6824c2ee07e83695
hemanthkolli031 <hemanthsai797@gmail.com>
2023-07-06 10:19:10 +0530

    delay

---
+++ python0507.py
@@ -0,0 +1,51 @@
+# for i in range(100):
+#     if i % 10 != 0:
+#         print(i)
+    
+    
+
+
+# for i in range(100):
+#     if i % 10 != 0:
+#         print(i)
+#     if i % 10 == 0:
+#         print(i)
+#     else:pass
+    
+    
+
+# x=10
+# print(x)
+# del x
+
+
+    
+# s="hemanth"  
+# del s(0)
+
+
+
+# x=10
+# y=20
+# del x,y
+# print(x)
+    
+    
+
+# x=10
+# y=20
+# del(x)
+# y=None
+# print(y)
+
+
+
+s1="hemanth"
+s2="hemanth"
+s3="hemanth"
+del s1
+print(s2,s3)
+
+
+    
+    
