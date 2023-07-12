306fc69bf6d7619eb6171a3d62b33101c1873d69
hemanthkolli031 <hemanthsai797@gmail.com>
2023-07-12 18:31:08 +0530

    list

---
+++ python1207.py
@@ -0,0 +1,321 @@
+# print('Hemanth786'.isalnum())
+# print('Hemanth786'.isalpha())
+# print('Hemanth'.isalpha())
+# print('Hemanth'.isdigit())
+# print('485030489'.isdigit())
+# print('hemanth'.islower())
+# print('Hemanth'.islower())
+# print('hemanth8279'.islower())
+# print('HEMANTH'.isupper())
+# print('Hemanth Kolli'.istitle())
+# print('Hemanth kolli'.istitle())
+# print('     '.isspace())
+
+
+
+
+# s=input("enter any character:")
+# if s.isalnum():
+#     print("it is alpha numeric character")
+#     if s.isalpha():
+#         print("it is alphabet symbol")
+#         if s.islower():
+#             print("it is lower case alphabet Symbol")
+#         else:
+#             print("it is upper case alphabet Symbol")
+#     else:
+#         print("its a digit")
+# elif s.isspace():
+#     print("it is a space character")
+# else:
+#     print("its is a non space character")
+ 
+ 
+ 
+# l=[]
+# l.append(10) 
+# l.append("hemanth")
+# l.append(10.5)
+# print(l)
+# l[0]=77
+# print(l)
+
+
+
+# l=[]
+# print(type(l))
+
+
+
+# l=eval(input("enter the list:"))
+# print(l)
+# print(type(l))
+
+
+
+# l=list('hemanth')
+# print(l)
+
+
+
+# l=list(range(0,10,2))
+# print(l)
+
+
+
+# l=list(range(0,10,3))
+# print(l)
+
+
+
+# s="learning python is easy"
+# l=s.split()
+# print(l)
+# print(type(l))
+
+
+
+# l=[10,20,30,40]
+# print(l[0])
+# print(l[-1])
+# print(l[100])
+
+
+
+# l=[10,20,30,40,50,60,70,80,90,100] 
+# print(l[2:7])
+# print(l[2:7:2])  
+# print(l[4::2]) 
+# print(l[8:2:-2])  
+# print(l[4:100])
+# print(l[4:0:2])  
+# print(l[::1])
+# print(l[4:0:-2]) 
+# print(l[::-1])
+
+
+
+# l=[0,1,2,3,4,5,6,7,8,9,10]
+# i=0
+# while  i<len(l):
+#     print(l[i])
+#     i=i+1
+
+
+
+# l=[0,1,2,3,4,5,6,7,8,9,10]
+# for x in l:
+#     print(x)
+
+
+
+# l=[0,1,2,3,4,5,6,7,8,9,10]
+# for x in l:
+#     if x % 2 == 0:
+#         print(x)
+
+
+
+# l=[0,1,2,3,4,5,6,7,8,9,10]
+# for x in l:
+#     if x % 2 == 1:
+#         print(x)
+
+
+
+# l=[10,20,30,40,50,60]
+# i=0
+# for x in l:
+#     print("the no present at",i,"index is",x)
+#     i=i+1
+
+
+
+# l=[10,20,30,40,50,60]
+# i=0
+# while i < len(l):
+#     print("the element present at +ve index:{} and at -ve index:{} is :{}".format(i,i-len(l),l[i]))
+#     i=i+1 
+
+
+
+# l1=[10,20,30,40]
+# l2=[50,60,70,80]
+# l3=l1+l2
+# print(l3)
+
+
+
+# l1=[10,20,30,40]
+# l2=[50,60,70,80]
+# l3=l1*l2
+# print(l3)
+
+
+
+# l1=[10,20,30,40]
+# l2=l1+40
+# print(l2)
+# l2=l1+[50]
+# print(l2)
+
+
+
+# l1=[10,20]
+# l2=l1*2
+# print(l2)        
+
+
+
+# l1=[10,20]
+# l2=l1*2.0
+# print(l2)      
+
+
+
+# l1=[10,20]
+# l2=l1*l1
+# print(l2)        
+
+
+
+# l1=[10,20]
+# l2=[30,40]
+# l3=l1+l2
+# l4=l3*3
+# print(l4) 
+
+
+
+
+# l1=["Dog","Cat","Rat"]
+# l2=["Dog","Cat","Rat"]
+# l3=["DOG","CAT","RAT"]
+# l4=["Cat","Rat","Dog"]
+# print(l1==l2)
+# print(l1==l3)
+# print(l1==l4)
+# print(l1!=l4)
+
+
+
+# l1=[10,20,30,40]
+# l2=[50,60]
+# print(l1<l2)
+# print(l1<=l2)
+# print(l1>l2)
+# print(l1>=l2)
+
+
+
+
+# l1=[50,60]
+# l2=[10,20,30,40]
+# print(l1<l2)
+# print(l1<=l2)
+# print(l1>l2)
+# print(l1>=l2)
+
+
+
+# l1=[10,20,30,40]
+# l2=[5,60]
+# print(l1<l2)
+# print(l1<=l2)
+# print(l1>l2)
+# print(l1>=l2)
+
+
+
+
+# l1=[10,20,30,40]
+# l2=[10,20,30,5]
+# print(l1<l2)
+# print(l1<=l2)
+# print(l1>l2)
+# print(l1>=l2)
+
+
+
+
+# l1=[10,20,30,40]
+# l2=[10,20,30,40,50,60]
+# print(l1<l2)
+# print(l1<=l2)
+# print(l1>l2)
+# print(l1>=l2)
+
+
+
+# l1=["hemanth","kolli"]
+# l2=["anna" "de arms"]
+# print(l1<l2)
+# print(l1<=l2)
+# print(l1>l2)
+# print(l1>=l2)
+
+
+
+
+# l1=[10,20,30,40]
+# print(10 in l1)
+# print(50 not in l1)
+# print(70 in l1)
+
+
+
+# l=[10,20,30]
+# print(len(l))
+# print(sorted(l))
+
+
+
+# l=[10,20,10,20,30,20,40]
+# print(l.count(10))
+# print(l.count(20))
+# print(l.count(50))
+
+
+
+# l=[1,2,1,2,3,4]
+# print(l.index(1))
+# print(l.index(2))
+# print(l.index(4))
+# print(l.index(5))
+
+
+
+l=[1,2,2,2,3,3]
+x=int(input("enter element to find:"))
+if x in l:
+    print("{} present at index:{}".format(x,l.index(x)))
+else:
+    print(x,"not available in the list")
+
+
+
+
+
+
+
+
+
+
+
+ 
+ 
+
+ 
+
+
+ 
+
+
+
+
+
+
+
+
+
+
